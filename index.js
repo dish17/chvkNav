41f5a6935dedbd6baa209fbe5f9f72fdba787153
dish17 <kimanocisc@gmail.com>
2023-05-15 16:07:56 +0300

    refWorks

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-function open(ref) {
-    function goToOtherPage() {
-        window.location.href = ref;
-    }
 
-    window.setTimeout(goToOtherPage, 1000);
-}
+document.getElementById("telegram").addEventListener('click', () => {
+    document.location.href = "https://t.me/python_is_trash";
+});
+
+
+document.getElementById("telegramChat").addEventListener('click', () => {
+    document.location.href = "https://t.me/+hwSA2qlxgPc4OWU6";
+});
+
+
+document.getElementById("tiktok").addEventListener('click', () => {
+    document.location.href = "https://www.tiktok.com/@python_is_trash?_t=8cL1VhBQ6bt&_r=1";
+});
+
+document.getElementById("discord").addEventListener('click', () => {
+    document.location.href = "https://discord.com/servers/python-is-community-1085170832545955932";
+});
